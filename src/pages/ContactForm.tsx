42da60a33b81f05411b774f1cb6753f819559cee
Sgreamss <earthwitchapol2@gmail.com>
2024-09-12 00:55:32 +0700

    Updated contactFormPage

--- src/pages/ContactForm.tsx
+++ src/pages/ContactForm.tsx
@@ -1,11 +1,48 @@
 import FrontLayout from "../layouts/FrontLayout"
 
 
+
 function ContactForm() {
   return (
     <FrontLayout>
-        <div>
+        <div className="max-w-md mx-auto p-4 bg-grey rounded-lg shadow-md">
             <h1 className="text-4xl font-bold text-center">Contact Form</h1>
+            
+            <form >
+                <div className="flex flex-col justify-center">
+
+                  <label htmlFor="name" className="block mt-2">First Name</label>
+                  <div className="mt-1">
+                    <input
+                      id="first-name"
+                      name="first-name"
+                      type="text"
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <label htmlFor="surname" className="mt-2">Last Name</label>
+                  <div className="mt-1">
+                    <input
+                      id="last-name"
+                      name="last-name"
+                      type="text"
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <label htmlFor="age">Age</label>
+                  <div className="mt-1 mb-2">
+                    <input
+                      id="age"
+                      name="age"
+                      type="number"
+                      className="block w-1/2 rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <button type="button" className=" align-middle w-16 h-8 rounded-md bg-gray-800 px-2 py-1 text-sm text-white hover:shadow-md hover:bg-gray-600">
+                    Submit
+                  </button>
+                </div>
+            </form>
         </div>
     </FrontLayout>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,6 @@ function Home() {
             </div>
             <h1 className="text-4xl font-bold text-center">Home Page</h1>
             <p className="text-center">Welcome to the home page</p>
-            
         </div>
     </FrontLayout>
   )
